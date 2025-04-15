06d6cb495d36c5b44e25af7b018961f3d2495264
GChuf <gasper.cefarin@gmail.com>
2025-04-15 15:36:09 +0200

    wip

--- artemis-console-extension/artemis-extension/webpack.config.cjs
+++ artemis-console-extension/artemis-extension/webpack.config.cjs
@@ -43,20 +43,7 @@ module.exports = (webpackEnv, args) => {
       new HtmlWebpackPlugin(Object.assign({}, {
         inject: true,
         template: path.resolve(__dirname, 'public/index.html')
-      }, isEnvProduction ? {
-        minify: {
-          removeComments: true,
-          collapseWhitespace: true,
-          removeRedundantAttributes: true,
-          useShortDoctype: true,
-          removeEmptyAttributes: true,
-          removeStyleLinkTypeAttributes: true,
-          keepClosingSlash: true,
-          minifyJS: true,
-          minifyCSS: true,
-          minifyURLs: true
-        }
-      } : undefined)),
+      })),
       new InterpolateHtmlPlugin(HtmlWebpackPlugin, {
         PUBLIC_URL: '/console'
       }),
